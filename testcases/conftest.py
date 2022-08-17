c82cdc6405ec7b841a3b72e395a1c193526d6d04
kinopico <zqmillet@qq.com>
2022-08-17 15:13:35 +0000

    [Test #1] finish breakpoint

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,6 +1,27 @@
+from typing import Type
+from typing import TypeVar
+from socket import socket
+from socket import AF_INET
+from socket import SOCK_STREAM
+from multiprocessing import Process
+from time import sleep
+
+from uvicorn import run
+from fastapi import FastAPI
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm.decl_api import DeclarativeMeta
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
 from pytest import fixture
 from _pytest.config.argparsing import Parser
 from _pytest.fixtures import SubRequest
+
+from api_genius import Router
+
+def is_open_port(port: int) -> bool:
+    with socket(AF_INET, SOCK_STREAM) as sock:
+        return sock.connect_ex(('localhost', port)) == 0
 
 def pytest_addoption(parser: Parser):
     parser.addoption(
@@ -43,6 +64,14 @@ def pytest_addoption(parser: Parser):
         help='specify the database name of mysql for test'
     )
 
+    parser.addoption(
+        '--server-port',
+        type=int,
+        action='store',
+        default=1926,
+        help='specify the port of the server under test'
+    )
+
 @fixture(name='mysql_host', scope='session')
 def _mysql_host(request: SubRequest) -> str:
     return request.config.getoption('mysql_host')
@@ -62,3 +91,42 @@ def _mysql_password(request: SubRequest) -> str:
 @fixture(name='mysql_database', scope='session')
 def _mysql_database(request: SubRequest) -> str:
     return request.config.getoption('mysql_database')
+
+@fixture(name='server_port', scope='session')
+def _server_port(request: SubRequest) -> int:
+    return request.config.getoption('server_port')
+
+@fixture(name='base_class', scope='session')
+def _base_class() -> Type[DeclarativeMeta]:
+    return declarative_base()
+
+Base = TypeVar('Base')
+
+@fixture(name='user_model', scope='session')
+def _user_model(base_class: Type[Base]) -> Type[Base]:
+    class User(base_class): # type: ignore
+        __tablename__ = 'users'
+
+        id = Column(String(), primary_key=True)
+        name = Column(String())
+        age = Column(Integer())
+    return User
+
+@fixture(name='server', scope='function')
+def _server(user_model: Type[Base], server_port: int) -> None:
+    assert not is_open_port(server_port)
+
+    application = FastAPI()
+    router = Router()
+    router.add_tabulate_api(None, user_model)
+    application.include_router(router)
+
+    process = Process(target=run, args=(application,), kwargs={'host': '0.0.0.0', 'port': server_port, 'debug': True}, daemon=True)
+    process.start()
+
+    while not is_open_port(server_port):
+        sleep(0.5)
+
+    yield
+
+    process.terminate()
--- testcases/router/add_tabulate_api_test.py
+++ testcases/router/add_tabulate_api_test.py
@@ -1,2 +1,6 @@
-def test_add_tabulate_api_test(mysql_host: str, mysql_port: int, mysql_username: str, mysql_password: str) -> None:
-    print(mysql_host, mysql_port, mysql_username, mysql_password)
+from requests import get
+from api_genius import Router
+
+def test_add_tabulate_api_test(mysql_host: str, mysql_port: int, mysql_username: str, mysql_password: str, server, user_model, server_port) -> None:
+    print(get(f'http://localhost:{server_port}/').text)
+
