1abe515e3abce2d4b77ce5d59ea6082713130346
kinopico <zqmillet@qq.com>
2022-08-19 08:03:33 +0000

    [Chore #1] add the pylint configuration file.

--- api_genius/router/router.py
+++ api_genius/router/router.py
@@ -2,7 +2,14 @@ from fastapi import APIRouter
 from fastapi import Request
 
 class Router(APIRouter):
+    """
+    this class is a subclass of the class APIRouter of fastapi module.
+    it provides additional method for auto api generation.
+    """
     def add_tabulate_api(self, session_class, model):
+        """
+        this method is used to generate tabulate api of a model.
+        """
         @self.get('/')
         def get(request: Request) -> str:
             return 'hello world'
